a0ee44747aa2d3cb61ac7a479c21eb07e7426b5f
Anas Ali <anas.ali@phonepe.com>
2025-06-05 18:02:28 +0530

    adding prod test status polling

--- js/status.js
+++ js/status.js
@@ -1,5 +1,5 @@
-const defaultClientId = "SWIGGY8_123";
-const defaultClientCred = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCJVrpuLL8drSOGo5N3s89e8gwakggT1S9BaAtZQgDaE3h1szlWIGvLNuNN1UwoiZK7u37wgHbOJj21xAX5eOZ4iHCbXmoTiWmCLVys9Kw21MeZYV5Rx2tDtIsSZdb7nop7cD48H1GkbIy3OGqGXEUgWjYiAI7pvuhu8ukAWPHy005yLfnZg84sOVb1cBRb31TK0JGSy9mTTKZ48A1c0CFnVLCIwlbTtRxDn1PmpowBXGjtOtSaWrF7WPwUzUowHioB7hJaGRdSpq24jXnIsTmjrC1cVNQYGhJYUrKV50klDqVBLU57t7l1kOcrpWftLIwf15ZMeShsk5Aveb/S6ndJAgMBAAECggEAIy9a99ry45F15oqFv0cLu5FbVHtRInOdda5IKgfk2/ndBJpz+AK6Wwem2EvrBP+p3fq9HLRsu4fEi4q61R0KPpsF7mgT2Ql9RGKGubC9ZhbBRCRT27OxYVkhwAbjOvd1Fwa8iv6jFfLRYidL8zw0YYN7Ft75vhyww6vr0bD323OzES4jYTOGtAvpcTJbry4N13RauizRIVNeCZRe9KTZRFedfazj0nCYc1IxwqeuqjVdzgIAsUwioXJqlyZF24+qTlF2Uws/dZEr0gIPNHNfW2MaaaygMJmUzT/bO2eYQBMvKu83OOvS69C1DWO+hNT/28jTcHmsmrFR6WszMIJL4QKBgQDFs6zh4DN1OciFhM9Tx+92mJBOYJQvb4YBOPs5QNPlW87RyrPhlNw+48QixeZ/fhyIHBD94hW9yhVXGFvKQKME098nsMMp2Qt6/8+g8l8E2DEtSoS8XKXkm3q/Ze5V2mF8uXnhqENIsvXB+56MZqnobvdnywSViOMjQihQyuhFZQKBgQCx1k1OQ43DtthqSosmGcAJiUdlOEMkHWzw3IIR94Rc1rlJPU21/sWrlBVMMacNy40W9Dwf6rmQOeyFhcbWirwpRDNgyfDM/HKmZjH7Qe1F3C8xw3UCF9Md3jG/mivxMVBQiW7m5IvVACY3e5RiFpKrPdcymkLCk7Qi94wYdlhOFQKBgQCBs12hFghMSKtfxQCLc2iLQo2xjbTJ5f1hAfP23KvnFbL6eWRHgvR3IdAEDBcq3x5ywQ74sIEY8OHNMMLPTlZkjWciT9nJs4XPnNGnQjrKH9crYm7wcEu5TbNaq9GPa2R3gesO+uehYx7Ns3iRnaysO1h+NHd+br6s2/M9BdEZ7QKBgE43CtkZLQifKp/VGU4wlR2cmyIRlLYVmwx0b2CnlDeg2O01YTBiVV4ZSySv0eFnXS5zTN5cxjCAyV4QcFt7uTYNIOu45YfCoEo/OExhupG3PaqNZLD++YNxbj/u9tSl88T4LSav7jBIWIaee6yIcQmPsU44OpSJkfivF5bKthvhAoGBAK3B11f9APwE1C9q4p+af23lSYPo8UL1EUwZ8jINS8uEmf4Rbe6/VXKxJoBLRDtL1GQQ05D2M7JnAUxIqBcQOlKzqtJkl/NwZA5oUYUVW6LsEx4WHaXF7uiw1IXw4YRytdo5gX45/oAG7FwCmAyZnNFSwPrHoGY6IcLUG26bVgoe"
+const defaultClientId = "SU2407261558245785595105";
+const defaultClientCred = "e5ceadd4-d259-400e-a6b8-0de5a89f5674"
 
 const loaderElement = document.getElementById("loader-animation");
 const successAnimationElement = document.getElementById("success-animation");
@@ -50,7 +50,7 @@ class OrderStatusFetcher {
   
     async fetchOrderStatus(callback) {
       try {
-        const urlWithId = `https://api-testing.phonepe.com/apis/b2b-pg/payments/v2/order/${this.orderId}/status`;
+        const urlWithId = `https://api.phonepe.com/apis/pg/payments/v2/order/${this.orderId}/status`;
         console.log(`Fetching status for order ${this.orderId} from: ${urlWithId}`);
         console.log("token: ", this.token);
         const response = await fetch(urlWithId, {
@@ -99,7 +99,7 @@ async function getOlympusToken(clientConfig) {
   params.append('client_secret', clientCred);
   params.append('grant_type', 'client_credentials');
   // ?client_id=${clientId}&client_version=1&client_secret=${clientCred}&grant_type=client_credentials
-  const response = await fetch(`https://api-testing.phonepe.com/apis/identity-manager/v1/oauth/token`, {
+  const response = await fetch(`https://api.phonepe.com/apis/identity-manager/v1/oauth/token`, {
     method: "POST",
     headers: {
       "accept": "application/json",
